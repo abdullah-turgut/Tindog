3526389a5e82ea6e3cd05c7b92bf7c04781f9037
abdullah-turgut <abdullah.turgut.94@gmail.com>
2022-11-20 19:45:11 +0300

    create js and finish

--- data.js
+++ data.js
@@ -1,29 +1,50 @@
 const dogs = [
-    {
-        name: "Rex",
-        avatar: "images/dog-rex.jpg",
-        age: 25,
-        bio: "Art. Literature. Natural wine. Yoga.",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },{
-        name: "Bella",
-        avatar: "images/dog-bella.jpg",
-        age: 43,
-        bio: "Yup, that's my owner. U can meet him if you want",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    },
-    {
-        name: "Teddy",
-        avatar: "images/dog-teddy.jpg",
-        age: 30,
-        bio: "How you doin?",
-        hasBeenSwiped: false,
-        hasBeenLiked: false
-    }
-]
+  {
+    name: 'Rex',
+    avatar: 'images/dog-rex.jpg',
+    age: 25,
+    bio: 'Art. Literature. Natural wine. Yoga.',
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: 'Bella',
+    avatar: 'images/dog-bella.jpg',
+    age: 43,
+    bio: "Yup, that's my owner. U can meet him if you want",
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+  {
+    name: 'Teddy',
+    avatar: 'images/dog-teddy.jpg',
+    age: 30,
+    bio: 'How you doin?',
+    hasBeenSwiped: false,
+    hasBeenLiked: false,
+  },
+];
 
+class Dog {
+  constructor(data) {
+    Object.assign(this, data);
+  }
+  createHtml() {
+    const { name, avatar, age, bio, hasBeenSwiped, hasBeenLiked } = this;
 
-export default dogs
+    return `<img
+          src="${avatar}"
+          alt="dog image"
+          class="profile-image"
+      />
+      <img src="" alt="swipe badge" class="badge hidden" />
+      <div class="profile-info">
+          <p class="name-age">${name}, ${age}</p>
+          <p class="bio">${bio}</p>
+      </div>`;
+  }
+}
+
+export default dogs;
+export { Dog };
 // Remember to export the data!
--- index.js
+++ index.js
@@ -1,4 +1,52 @@
 // Remember to import the data and Dog class!
 import dogs from './data.js';
+import { Dog } from './data.js';
 
-console.log(dogs);
+const btnDislike = document.querySelector('#btn-dislike');
+const btnLike = document.querySelector('#btn-like');
+const profile = document.querySelector('.profile');
+
+const dogsArray = dogs.map((dog) => new Dog(dog));
+console.log(dogsArray);
+
+function renderHtml(index) {
+  profile.innerHTML = dogsArray[index].createHtml();
+}
+
+function toggleLikeBadge(index) {
+  let img = document.querySelector('.badge');
+  dogsArray[index].hasBeenLiked = true;
+  dogsArray[index].hasBeenSwiped = false;
+  img.setAttribute('src', 'images/badge-like.png');
+  img.classList.remove('hidden');
+}
+
+function toggleNopeBadge(index) {
+  let img = document.querySelector('.badge');
+  dogsArray[index].hasBeenLiked = false;
+  dogsArray[index].hasBeenSwiped = true;
+  img.setAttribute('src', 'images/badge-nope.png');
+  img.classList.remove('hidden');
+}
+
+let index = 0;
+
+renderHtml(index);
+
+btnLike.addEventListener('click', () => {
+  toggleLikeBadge(index);
+
+  index === dogs.length - 1 ? (index = 0) : index++;
+  setTimeout(() => {
+    renderHtml(index);
+  }, 1000);
+});
+
+btnDislike.addEventListener('click', () => {
+  toggleNopeBadge(index);
+
+  index === dogs.length - 1 ? (index = 0) : index++;
+  setTimeout(() => {
+    renderHtml(index);
+  }, 1000);
+});
